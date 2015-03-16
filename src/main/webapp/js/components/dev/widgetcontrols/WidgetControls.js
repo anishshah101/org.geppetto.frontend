0a14f503a6468e1cd55f049297ecdd8e7383bcd9
Anish Shah <anishshah101@gmail.com>
2015-03-16 07:02:59 +0530

    Created the widget toolbar js files

---
+++ src/main/webapp/js/components/dev/widgetcontrols/WidgetControls.js
@@ -0,0 +1,81 @@
+define(function(require) {
+
+    var React = require('react');
+    var GEPPETTO = require('geppetto');
+
+    var Controls = React.createClass({
+
+        mixins:[require('mixins/TutorialMixin')],
+
+        popoverTitle: 'Widget Controls',
+
+        popoverText: 'Use these controls to add or remove a widget.',
+
+        popoverTemplate: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div><button class="btn btn-info tutorial-next"><i class="icon-check"></i></button></div>',
+
+        w1: function() {
+            GEPPETTO.Console.executeCommand('G.addWidget(0)');
+        },
+
+	   w2: function() {
+            GEPPETTO.Console.executeCommand('G.addWidget(1)');
+        },
+        
+        w3: function() {
+            GEPPETTO.Console.executeCommand('G.addWidget(2)');
+        },
+        
+        w4: function() {
+            GEPPETTO.Console.executeCommand('G.addWidget(3)');
+        },
+        
+        w5: function() {
+            GEPPETTO.Console.executeCommand('G.addWidget(4)');
+        },
+        
+        w6: function() {
+            GEPPETTO.Console.executeCommand('G.addWidget(5)');
+        },
+        	 
+        componentDidMount: function() {
+            GEPPETTO.on('start:tutorial', (function() {               
+                GEPPETTO.once('tutorial:cameracontrols', (function(){
+                    if(GEPPETTO.tutorialEnabled) {
+                        this.showPopover;
+                    }
+                }).bind(this)); 
+
+                $('.tutorial-next').click(function(){
+                    this.destroyPopover;
+                    GEPPETTO.trigger('tutorial:console');
+                }.bind(this));
+            }).bind(this));
+        },
+
+        render: function () {
+            return (
+            	<div className="position-toolbar">
+                    <button className="btn squareB icon-chevron-left pan-left" onClick={this.panLeft}></button>
+                    <button className="btn squareB icon-chevron-up pan-top" onClick={this.panUp}></button>
+                    <button className="btn squareB icon-chevron-right pan-right" onClick={this.panRight}></button>
+                    <button className="btn squareB icon-chevron-down pan-bottom" onClick={this.panDown}></button>
+                    <button className="btn squareB icon-home pan-home" onClick={this.cameraHome}></button>
+
+                    <button className="btn squareB icon-undo rotate-left" onClick={this.rotateLeft}></button>
+                    <button className="btn squareB icon-repeat rotate90 rotate-top" onClick={this.rotateUp}></button>
+                    <button className="btn squareB icon-repeat rotate-right" onClick={this.rotateRight}></button>
+                    <button className="btn squareB icon-undo rotate90 rotate-bottom" onClick={this.rotateDown}></button>
+                    <button className="btn squareB icon-home rotate-home" onClick={this.cameraHome}></button>
+
+                    <button className="btn squareB icon-zoom-in zoom-in" onClick={this.zoomIn}></button>
+                    <button className="btn squareB icon-zoom-out zoom-out" onClick={this.zoomOut}></button>
+                </div>
+
+            );
+        }
+
+    });
+
+    React.renderComponent(Controls({},''), document.getElementById('camera-controls'));
+
+});
